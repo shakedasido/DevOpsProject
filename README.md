bea5ade0364670675c2e6d8a4d3f81e26c1d7dd4
MichaelKla1 <michakl1@gmail.com>
2023-05-29 23:37:17 +0300

    changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# DevOpsProject
+In this repository (first sprint) we set up the server and added home page and login page.
+
+Set up and run project:
+
+Before opening and running the project, you will need to have Node.js installed on your computer. 
+Additionally, you will need an Integrated Development Environment (IDE) or a Command Terminal to work with the
+project. Having these tools in place will enable you to open and run the project on your computer.
+
+ - npm install
+ - npm start
+ 
+In order to run these commands, it is required to install Node.js version and IDE or Command terminal on the computer.
+Definitions, dependencies, and more information about the project can be found in the file Package.json.
---
+++ src/index.js
@@ -0,0 +1,129 @@
+import express from 'express';
+import pkg from 'pg';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+
+const { Client } = pkg;
+
+const client = new Client({
+   user: 'shakeda',
+   host: 'dpg-cgugbmg2qv2fdedbo640-a.frankfurt-postgres.render.com',
+   database: 'juniyoudb',
+   password: 'puMtqvjlq7ftkPfuvHwZFKUuZ5tlWQiV',
+   port: 5432,
+   ssl: true,
+});
+
+client.connect();
+const port = process.env.PORT || 80;
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+   secret: 'asdfkml5rtythytt6onjghfojfdpflhplp9trd7htru5u4969u',
+   saveUninitialized: false,
+   resave: false,
+}));
+
+function clean(str) {
+   for (let i = 0; i < str.length; i += 1) {
+      if (str[i] < 'a' || str[i] > 'z') {
+         if (str[i] < 'A' || str[i] > 'Z') {
+            if (str[i] < '0' || str[i] > '9') {
+               if (str[i] !== '@' && str[i] !== '.') {
+                  return false;
+               }
+            }
+         }
+      }
+   }
+   return true;
+}
+
+app.get('/', (req, res) => {
+   res.sendFile('page/index.html', { root: './' });
+});
+
+app.get('/getstyle', (req, res) => {
+   res.sendFile('style/style.css', { root: './' });
+});
+
+app.get('/getjs', (req, res) => {
+   res.sendFile('js/scripts.js', { root: './' });
+});
+
+app.post('/update_post_data', async (req, res) => {
+
+   const data = req.body;
+   const webData = data;
+   if (webData.action !== undefined && webData.action === 'delete') {
+      if (webData.post !== undefined && clean(webData.post)) {
+         client.query('DELETE FROM grades WHERE uid= $1', [webData.post]);
+         const obj = '{"response": "0"}';
+         res.send(obj);
+      }
+   }
+
+});
+
+//The server return table of grades
+app.post('/page_loader', async (req, res) => {
+   const data = req.body;
+   const webData = data;
+   if (webData.info !== undefined && webData.info === 'registerpage') {
+      var strToSend = "";
+      strToSend = `${strToSend}<div style=\\"text-align:center;\\">`;
+      let res1 = await client.query('SELECT * FROM grades'); // DATABASE CONNECTION ARE IN DIFFERENT THREADS, NEED MANUAL TERAPY
+      strToSend += '<table border=\\"1\\"><tr><td>Student name</td><td>Exam 1</td><td>Exam 2</td><td>Exam 3</td><td>Delete</td></tr>';
+      for (let i = 0; i < res1.rows.length; i += 1) {
+         strToSend += '<tr>';
+         // build the server
+         strToSend = `${strToSend}<td>${res1.rows[i].username}</td><td>${res1.rows[i].exam1}</td><td>${res1.rows[i].exam2}</td><td>${res1.rows[i].exam3}</td><td><input type=\\"button\\" value=\\"Delete\\" onclick=\\"Delete('${res1.rows[i].uid}')\\"></td>`;
+         strToSend += '</tr>';
+      }
+      //strToSend += '<tr>';
+      //strToSend += '<td><input type="text" maxlength="30" placeholder="Username" id="username"></td></td><td></td><td></td>';
+      //strToSend += '</tr>';
+      strToSend += '</table>';
+      strToSend += '</div>';
+      const obj = `{"response": "${strToSend}"}`;
+      res.send(obj);
+   }
+});
+
+app.post('/process_post_req', async (req, res) => {
+   try {
+      // Get the JSON data from the request
+      const data = req.body;
+      const webData = data;
+      const type = webData.action;
+
+      // If it's a true statement, then we can send the POST request
+      // Register
+      if (type !== undefined && type === 'process_register' && webData.username !== undefined && webData.exam1 !== undefined && webData.exam2 !== undefined && webData.exam3 !== undefined) {
+         const uname = webData.username;
+         const exam1 = webData.exam1;
+         const exam2 = webData.exam2;
+         const exam3 = webData.exam3;
+         let tBool = true;
+
+         if (uname.length < 3 || uname.length > 30 || isNaN(parseInt(exam1)) || parseInt(exam1) < 0 || parseInt(exam1) > 100 || isNaN(parseInt(exam2)) || parseInt(exam2) < 0 || parseInt(exam2) > 100 || isNaN(parseInt(exam3)) || parseInt(exam3) < 0 || parseInt(exam3) > 100 || exam1.length >3 || exam2.length >3 || exam3.length >3) {
+            tBool = false;
+         }
+         if (!clean(uname) && !clean(exam1) && !clean(exam2) && !clean(exam3)) {
+            tBool = false;
+         }
+         if (tBool) {
+            client.query('INSERT INTO grades (username, exam1, exam2, exam3) values($1, $2, $3, $4)', [uname, exam1, exam2, exam3]);
+            const obj = '{"response": "0"}';
+            res.send(obj);
+         }
+      }
+   } catch (err) {
+      console.error(err);
+   }
+});
+
+app.listen(port, () => {
+   console.log(`Listening on port ${port}`);
+});
