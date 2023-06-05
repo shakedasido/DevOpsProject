ec9507baf132936b31305133f1a8908eadffc18e
shakedasido <shakedasido@gmail.com>
2023-06-05 14:50:59 +0300

    updated tests

--- tests/Contents.spec.js
+++ tests/Contents.spec.js
@@ -2,17 +2,18 @@ import {expect} from "chai";
 import request from 'supertest';
 import {app} from '../src/index.js';
 
-describe('Test suit 1:', () => {
-  test('test 1:', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
-  });
-
-  test('test 2:', async () => {
-    const res = await request(app).get('/3023');
-    expect(res.statusCode).toEqual(404);
-  });
-});
+describe('Contents', () => {
+    describe('Main page', () =>{
+        it('Test 1', async () =>{
+            const res = await request(app).get('/');
+            expect(res.statusCode).to.be.eql(200);
+        });
+        it('Test 2', async () =>{
+            const res = await request(app).get('/3485');
+            expect(res.statusCode).to.be.eql(404);
+        });
+    })
+})
 
 
 
