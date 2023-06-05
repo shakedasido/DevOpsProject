ec66eee25290d81c301c86dcee9af51c40284b50
shakedasido <shakedasido@gmail.com>
2023-06-05 14:56:08 +0300

    updated tests

--- tests/Contents.spec.js
+++ tests/Contents.spec.js
@@ -2,18 +2,18 @@ import {expect} from "chai";
 import request from 'supertest';
 import {app} from '../src/index.js';
 
-describe('Contents', () => {
-    describe('Main page', () =>{
-        it('Test 1', async () =>{
-            const res = await request(app).get('/');
-            expect(res.statusCode).to.be.eql(200);
-        })
-        it('Test 2', async () =>{
-            const res = await request(app).get('/3485');
-            expect(res.statusCode).to.be.eql(404);
-        })
+
+describe('Main page', () =>{
+    it('Test 1', async () =>{
+        const res = await request(app).get('/');
+        expect(res.statusCode).to.be.eql(200);
+    })
+    it('Test 2', async () =>{
+        const res = await request(app).get('/3485');
+        expect(res.statusCode).to.be.eql(404);
     })
 })
+
 
 
 
