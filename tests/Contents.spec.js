621db1b4896f5c8c783d61d476d2560c3e192d0a
shakedasido <shakedasido@gmail.com>
2023-06-05 14:53:53 +0300

    updated tests

--- tests/Contents.spec.js
+++ tests/Contents.spec.js
@@ -7,11 +7,11 @@ describe('Contents', () => {
         it('Test 1', async () =>{
             const res = await request(app).get('/');
             expect(res.statusCode).to.be.eql(200);
-        });
+        })
         it('Test 2', async () =>{
             const res = await request(app).get('/3485');
             expect(res.statusCode).to.be.eql(404);
-        });
+        })
     })
 })
 
