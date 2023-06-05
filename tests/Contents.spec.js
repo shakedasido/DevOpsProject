272e1d338aca320e44aa3665ec7ef7450c53c97c
shakedasido <shakedasido@gmail.com>
2023-06-05 13:55:22 +0300

    added tests to project and fixed package.json file

--- tests/Contents.spec.js
+++ tests/Contents.spec.js
@@ -1,21 +1,18 @@
 import {expect} from "chai";
-import * as fs from "fs";
+import request from 'supertest';
+import app from '../server';
 
-describe('Contents', () => {
-    describe('Main page', () =>{
-        it('Site information', async () =>{
-            var ind = -1;
-           // const fs = module.constructor._load('fs');
-            var data  = fs.readFileSync('./page/index.html'); 
-            
-            ind = data.indexOf('We are a social network designated for juniors. Those who want to develop their skills and become the next seniors. Our goal is to help you make this dream come true!<br /><br />How? With us, you can easily access the most current job inquiries, job offers, and links to free and relevant study sites categorized by subject. And above all, you will be able to express yourself and create new friendships and opportunities! <br /><br />So, what are you waiting for? Sign up for free!');
-            
-            
-            
-            
-            expect(ind).not.to.be.eql(-1);
-        })
-    })
-})
+describe('Test suit 1:', () => {
+  test('test 1:', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test('test 2:', async () => {
+    const res = await request(app).get('/3023');
+    expect(res.statusCode).toEqual(404);
+  });
+});
+
 
 
